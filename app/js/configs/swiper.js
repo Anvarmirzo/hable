6d757edea4326c47d616e98584f18c53507abce7
Anvarmirzo <www.latvienhoasadrizeges@gmail.com>
2022-02-23 10:02:05 +0500

    knowledge base responsive

--- app/js/configs/swiper.js
+++ app/js/configs/swiper.js
@@ -16,8 +16,24 @@ export const swiperConfig = () => {
       },
       breakpoints: {
         320: {
-          slidesPerView: 2,
+          slidesPerView: 1.5,
           spaceBetween: 20
+        },
+        630: {
+          slidesPerView: 1.7,
+          spaceBetween: 20
+        },
+        715: {
+          slidesPerView: 1.9,
+          spaceBetween: 20
+        },
+        810: {
+          slidesPerView: 2.2,
+          spaceBetween: 20
+        },
+        900: {
+          spaceBetween: 20,
+          slidesPerView: 2.5
         },
         980: {
           slidesPerView: 2.6,
@@ -36,7 +52,7 @@ export const swiperConfig = () => {
 
 // breakpoint where swiper will be destroyed
 // and switches to a dual-column layout
-  const breakpoint = window.matchMedia('(max-width:720px)');
+  const breakpoint = window.matchMedia('(max-width:576px)');
 // keep track of swiper instances to destroy later
   let mySwiper;
   const breakpointChecker = function () {
